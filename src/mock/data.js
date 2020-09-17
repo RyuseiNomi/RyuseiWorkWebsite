b59516be5571cfa87bb454d0aeb91a9b5f49a108
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-17 11:13:21 +0900

    change website name and description

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Ryusei a work website', // e.g: 'Name | Developer'
+  lang: 'jp', // e.g: en, es, fr, jp
+  description: 'RyuseiのWork Website', // e.g: Welcome to my website
 };
 
 // HERO DATA
