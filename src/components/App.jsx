1831034e5ce28febedaf1a4f9a3daf6dd5b2a268
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-18 18:14:01 +0900

    add Work section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
+import Work from './Work/Work';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
 
@@ -29,6 +30,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Work />
       <Contact />
       <Footer />
     </PortfolioProvider>
