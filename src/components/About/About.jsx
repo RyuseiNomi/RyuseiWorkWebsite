0dea3e1cb7ebb38645ca22fcbf3d9a0c868d9ceb
RyuseiNomi <kanontonto1010@gmail.com>
2020-10-07 23:39:07 +0900

    change Keywords style on AboutMe

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,16 +1,38 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
+import { makeStyles } from '@material-ui/core/styles'
+import Chip from '@material-ui/core/Chip';
+import ComputerIcon from '@material-ui/icons/Computer';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, name, paragraphOne, paragraphTwo, paragraphThree, qiita, resume } = about;
+  const { keywords } = useContext(PortfolioContext);
+  const { img, name, subTitle, paragraphOne, paragraphTwo, paragraphThree, qiita, resume } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+
+  /*
+   * Keyword表示用のChip向けStyle
+   * https://material-ui.com/components/chips/#chip
+   */
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(1.0),
+        padding: theme.spacing(1.0),
+        fontSize: '1.5rem',
+      },
+    },
+  }));
+  const keywordChipStyle = useStyles();
 
   useEffect(() => {
     if (window.innerWidth > 769) {
@@ -40,17 +62,26 @@ const About = () => {
                 <p className="about-wrapper__info-text--name">
                   {name || 'Ryusei Nomi'}
                 </p>
+                <p className="about-wrapper__info-text--subTitle">
+                  {subTitle || 'Non subtitle'}
+                </p>
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
                     'No Description'}
                 </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    ''}
-                </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphThree || ''}
-                </p>
+                <div className={keywordChipStyle.root}>
+                  {keywords.map((keyword) => {
+                    const { name, isHobby } = keyword;
+                    if (!isHobby) {
+                      return (
+                        <Chip icon={<ComputerIcon />} label={name} />
+                      );
+                    }
+                    return (
+                      <Chip label={name} />
+                    );
+                  })}
+                </div>
                 {qiita && (
                   <span className="d-flex mt-3">
                     <a
